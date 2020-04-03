2925866aa3a792a1d137c704e5fe5e10def67665
lida <lida@hulai.com>
2020-04-03 17:54:00 +0800

    update

--- learning-code/src/main/java/com/lee/timer/two/JobFactory.java
+++ learning-code/src/main/java/com/lee/timer/two/JobFactory.java
@@ -20,6 +20,7 @@ public abstract class JobFactory implements Job {
 
     /**
      * 私有的方法子类是无法继承的，也不存在覆盖
+     * 我们不可能完全继承父母的一切（如性格等），但是父母的一些无法继承的东西却仍会深刻的影响着我们。
      * Abstract和private也是不可以一起使用的
      * @param scheduler
      * @param commonJob
