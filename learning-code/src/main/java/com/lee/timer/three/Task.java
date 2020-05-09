3374f3c6a7b65a8528b9e67608a38ed062d41693
lida <lida@hulai.com>
2020-05-09 18:18:18 +0800

    update

--- learning-code/src/main/java/com/lee/timer/three/Task.java
+++ learning-code/src/main/java/com/lee/timer/three/Task.java
@@ -1,31 +1,31 @@
-package com.lee.event.global;
+package com.lee.timer.three;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
-
 public abstract class Task implements Runnable {
-    
+
     protected static Logger logger = LoggerFactory.getLogger(Task.class);
-    
+
     public final String name;
     public final int delay;
     public final int period;
     public final TimeUnit unit;
-    
+
     public Task(String name, int delay, int period, TimeUnit unit) {
         this.name = name;
         this.delay = delay;
         this.period = period;
         this.unit = unit;
     }
-    
+
     public Object key() {
         return this;
     }
-    
+
+    @Override
     public final void run() {
         try {
             this.exec();
@@ -35,37 +35,37 @@ public abstract class Task implements Runnable {
     }
 
     public abstract void exec();
-    
+
     /**
      * delay by minutes
      */
     public static Task once(String name, int delay, Runnable runnable) {
         return new RunnaleTask(name, delay, -1, TimeUnit.MINUTES, runnable);
     }
-    
+
     public static Task once(String name, int delay, TimeUnit unit, Runnable runnable) {
         return new RunnaleTask(name, delay, -1, unit, runnable);
     }
-    
-    
+
+
     /**
      * delay && period by 1 minutes
      */
     public static Task period(String name, Runnable runnable) {
         return new RunnaleTask(name, 1, 1, TimeUnit.MINUTES, runnable);
     }
-    
+
     /**
      * period by minutes
      */
     public static Task period(String name, int period, Runnable runnable) {
         return new RunnaleTask(name, period, period, TimeUnit.MINUTES, runnable);
     }
-    
+
     public static Task period(String name, int delay, int period, TimeUnit unit, Runnable runnable) {
         return new RunnaleTask(name, delay, period, unit, runnable);
     }
-    
+
     static class RunnaleTask extends Task {
         final Runnable runnable;
         public RunnaleTask(String name, int delay, int period, TimeUnit unit, Runnable runnable) {
